7bc2e237c6af654cffaf854349cf7325839b4487
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-06 23:43:55 +0800

    Add resize and clear canvas feature

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const canvas = document.querySelector('.canvas');
 const clearBtn = document.querySelector('.clearBtn');
 const slider = document.getElementById('slider');
+let cells = document.querySelectorAll('.cell');
 
 function createCanvas(size) {
-    const canvasDimensions = size * size;
+    const canvasDimension = size * size;
     canvas.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
 
-    for (i = 1; i <= canvasDimensions; i++) {
+    for (i = 1; i <= canvasDimension; i++) {
         const cell = document.createElement('div');
         cell.setAttribute('class', 'cell');
         document.querySelector('.canvas').appendChild(cell);
     }
+
+    cells = document.querySelectorAll('.cell');
 }
 
 function resetCanvas() {
@@ -20,27 +23,29 @@ function resetCanvas() {
         canvas.removeChild(child);
         child = canvas.lastElementChild;
     }
+
+    cells = document.querySelectorAll('.cell');
 }
 
-slider.oninput = function() {
-    resetCanvas();
-    createCanvas(this.value);
-}
-
-createCanvas(slider.value);
-
-const cells = document.querySelectorAll('.cell');
-
-// Listens for mouse over event and change the cell's color
-cells.forEach(function(cell) {
-    cell.addEventListener('mouseover' || 'clicked', function(e) {
-        e.target.classList.add('hovered');
+function listen() {
+    cells.forEach(function(cell) {
+        cell.addEventListener('mouseover' || 'clicked', function(e) {
+            e.target.classList.add('hovered');
+        });
     });
-});
+}
 
-// When clear canvas button is clicked, reset the cell's color
 clearBtn.addEventListener('click', function() {
     cells.forEach(function(cell) {
         cell.classList.remove('hovered');
     });
 });
+
+slider.addEventListener('change', function() {
+    resetCanvas();
+    createCanvas(this.value);
+    listen();
+});
+
+createCanvas(16);
+listen();
