ac3fb7339a7d7241678ea6b00892dc57e1689ba4
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-06 18:34:07 +0800

    Add clear canvas button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+const canvas = document.createElement('div');
+const container = document.querySelector('.container');
+const clearBtn = document.querySelector('.clearBtn');
+
+canvas.setAttribute('class', 'canvas');
+container.appendChild(canvas);
+
 function createCanvas(size) {
     const canvasDimensions = size * size;
-    const canvas = document.createElement('div');
-    const container = document.querySelector('.container');
-
-    canvas.setAttribute('class', 'canvas');
-    container.appendChild(canvas);
 
     for (i = 1; i <= canvasDimensions; i++) {
         const cell = document.createElement('div');
@@ -19,8 +21,16 @@ createCanvas(32);
 
 const cells = document.querySelectorAll('.cell');
 
+// Listens for mouse over event and change the cell's color
 cells.forEach(function(cell) {
     cell.addEventListener('mouseover', function(e) {
         e.target.classList.add('clicked');
+    });
+});
+
+// When clear canvas button is clicked, reset the cell's color
+clearBtn.addEventListener('click', function() {
+    cells.forEach(function(cell) {
+        cell.classList.remove('clicked');
     });
 });
