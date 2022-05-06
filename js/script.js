58a3a6c57e98372513221c3efd4c8c9af751593a
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-06 22:29:42 +0800

    Add slider functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-const canvas = document.createElement('div');
-const container = document.querySelector('.container');
+const canvas = document.querySelector('.canvas');
 const clearBtn = document.querySelector('.clearBtn');
-
-canvas.setAttribute('class', 'canvas');
-container.appendChild(canvas);
+const slider = document.getElementById('slider');
 
 function createCanvas(size) {
     const canvasDimensions = size * size;
+    canvas.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
 
     for (i = 1; i <= canvasDimensions; i++) {
         const cell = document.createElement('div');
@@ -15,20 +13,34 @@ function createCanvas(size) {
     }
 }
 
-createCanvas(32);
+function resetCanvas() {
+    let child = canvas.lastElementChild;
+    
+    while(child) {
+        canvas.removeChild(child);
+        child = canvas.lastElementChild;
+    }
+}
+
+slider.oninput = function() {
+    resetCanvas();
+    createCanvas(this.value);
+}
+
+createCanvas(slider.value);
 
 const cells = document.querySelectorAll('.cell');
 
 // Listens for mouse over event and change the cell's color
 cells.forEach(function(cell) {
-    cell.addEventListener('mouseover', function(e) {
-        e.target.classList.add('clicked');
+    cell.addEventListener('mouseover' || 'clicked', function(e) {
+        e.target.classList.add('hovered');
     });
 });
 
 // When clear canvas button is clicked, reset the cell's color
 clearBtn.addEventListener('click', function() {
     cells.forEach(function(cell) {
-        cell.classList.remove('clicked');
+        cell.classList.remove('hovered');
     });
 });
