7e37f5b019f23b8f4e813cc6fdbf4aaf7bd8a928
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-07 00:46:01 +0800

    Add rainbow and black ink toggle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const canvas = document.querySelector('.canvas');
 const clearBtn = document.querySelector('.clearBtn');
+const rainbowBtn = document.querySelector('.colorBtn');
+const blackBtn = document.querySelector('.blackBtn');
 const slider = document.getElementById('slider');
+const label = document.querySelector('.label');
+const randomColor = ['red', 'blue', 'yellow', 'green', 'violet', 'orange', 'pink'];
 let cells = document.querySelectorAll('.cell');
 
 function createCanvas(size) {
+    label.textContent = `Canvas size: 20x20`;
     const canvasDimension = size * size;
     canvas.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
 
@@ -27,25 +32,49 @@ function resetCanvas() {
     cells = document.querySelectorAll('.cell');
 }
 
-function listen() {
+function listen(color) {
     cells.forEach(function(cell) {
         cell.addEventListener('mouseover' || 'clicked', function(e) {
-            e.target.classList.add('hovered');
+            switch(color) {
+                case 'black':
+                    e.target.style.setProperty('background', `black`);
+                    break;
+                case 'rainbow':
+                    e.target.style.setProperty('background', `${randomColor.at(Math.floor(Math.random() * randomColor.length))}`);
+                    break;
+                default:
+                    e.target.style.setProperty('background', `black`);
+                    break;
+            }
         });
     });
 }
 
+function changeLabel(value) {
+    label.textContent = `Canvas size: ${value}x${value}`;
+}
+
+rainbowBtn.addEventListener('click', function() {
+    listen('rainbow');
+}); 
+
+blackBtn.addEventListener('click', function() {
+    listen('black');
+}); 
+
 clearBtn.addEventListener('click', function() {
     cells.forEach(function(cell) {
         cell.classList.remove('hovered');
+        cell.style.removeProperty('background');
     });
 });
 
 slider.addEventListener('change', function() {
     resetCanvas();
     createCanvas(this.value);
+    changeLabel(this.value);
     listen();
 });
 
-createCanvas(16);
+createCanvas(20);
 listen();
