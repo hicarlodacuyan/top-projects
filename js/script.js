e0ef181c0b90795281965516752ad7a799b5ef21
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-06 18:08:44 +0800

    Add change color feature when clicked

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 function createCanvas(size) {
+    const canvasDimensions = size * size;
     const canvas = document.createElement('div');
     const container = document.querySelector('.container');
 
     canvas.setAttribute('class', 'canvas');
     container.appendChild(canvas);
 
-    for (i = 1; i <= size; i++) {
+    for (i = 1; i <= canvasDimensions; i++) {
         const cell = document.createElement('div');
         cell.setAttribute('class', 'cell');
         document.querySelector('.canvas').appendChild(cell);
     }
 
-    console.log(document.querySelector('.canvas'));
+    console.log(canvas);
 }
 
-createCanvas(260);
+createCanvas(20);
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(function(cell) {
+    cell.addEventListener('mousedown', function(e) {
+        e.target.classList.add('clicked');
+    });
+});
