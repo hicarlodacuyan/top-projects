5ed644c03361aa65c886b6f784a950306d92b2ff
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-22 18:34:16 +0800

    Add basic user input and clearInput function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 let myLibrary = [];
 let generatedHtml = '';
+const authorInput = document.getElementById('author');
+const titleInput = document.getElementById('title');
+const pageInput = document.getElementById('page');
+const addButton = document.getElementById('add');
 
 function Book(author, title, numberOfPages) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
+}
+
+function updateDisplay() {
+    for(let book of myLibrary) {
+        generatedHtml = `<div class="book">
+                            <div class="author">${book.author}</div>
+                            <div class="title">${book.title}</div>
+                            <div class="pages">${book.numberOfPages}</div>
+                        </div>`;
+    };
+
+    document.querySelector('.container').innerHTML += generatedHtml;
 }
 
 function addBookToLibrary(author, title, numberOfPages) {
@@ -15,18 +31,14 @@ function removeBookFromLibrary(index) {
     myLibrary.splice(index, 1);
 }
 
-function updateDisplay() {
-    for (let item of myLibrary) {
-        generatedHtml += `<div class="book">
-                            <div class="author">${item.author}</div>
-                            <div class="title">${item.title}</div>
-                            <div class="pages">${item.numberOfPages}</div>
-                        </div>`;
-    };
-
-    document.querySelector('.container').innerHTML = generatedHtml;
+function clearInput() {
+    authorInput.value = '';
+    titleInput.value = '';
+    pageInput.value = '';
 }
 
-addBookToLibrary('J. K. Rowling', 'Harry Potter and the Sorcerer\'s Stone', '223');
-addBookToLibrary('J. K. Rowling', 'Harry Potter and the Sorcerer\'s Stone', '223');
-addBookToLibrary('J. K. Rowling', 'Harry Potter and the Sorcerer\'s Stone', '223');
+addButton.addEventListener('click', function() {
+    addBookToLibrary(authorInput.value, titleInput.value, pageInput.value);
+    updateDisplay();
+    clearInput();
+});
