9a5ceffec2eb8fd26f370c3033cd087e8cfca904
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-05-28 16:23:11 +0800

    Add setting of player marker

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const xBtn = document.getElementById('btn_x');
+const oBtn = document.getElementById('btn_o');
+const resetBtn = document.getElementById('btn_reset');
+
 const gameboard = (() => {
     let state = [null, null, null,
                  null, null, null,
@@ -18,12 +22,62 @@ const gameboard = (() => {
     return {getState, getTurn, setState, setTurn};
 })();
 
-const Player = (marker) => {
-    const playerMarker = marker;
+const Player = () => {
+    let playerMarker = '';
     const score = 0;
+    const setPlayerMarker = (marker) => {
+        marker == 'x' ? playerMarker = 'x' : playerMarker = 'o';
+    };
     const getPlayerMarker = () => playerMarker;
     const setScore = () => score++;
     const getScore = () => score;
 
-    return {getPlayerMarker, getScore, setScore};
+    return {getPlayerMarker, setPlayerMarker, getScore, setScore};
 };
+
+const human = Player();
+const cpu = Player();
+
+const playRound = () => {
+    gameboard.setTurn();
+    
+    if (gameboard.getTurn() >= 9) {
+        // TODO: Display to the DOM, the game is tie
+        // Show restart button
+        console.log('Tie');
+    }
+    // Checks if current turn is odd
+    if (gameboard.getTurn() % 2 == 1) {
+        console.log('Player 1 turn');
+    } else {
+        console.log('Player 2 turn');
+    }
+
+    console.log(`Turn: ${gameboard.getTurn()}, Human: ${human.getPlayerMarker()}, CPU: ${cpu.getPlayerMarker()}`);
+};
+
+xBtn.addEventListener('click', (e) => {
+    if (human.getPlayerMarker() === '' && cpu.getPlayerMarker() === '') {
+        human.setPlayerMarker('x');
+        cpu.setPlayerMarker('o')
+    }
+
+    if (gameboard.getTurn() == 0) {
+        playRound();
+    }
+});
+
+oBtn.addEventListener('click', (e) => {
+    if (human.getPlayerMarker() === '' && cpu.getPlayerMarker() === '') {
+        human.setPlayerMarker('o');
+        cpu.setPlayerMarker('x')
+    }
+
+    if (gameboard.getTurn() == 0) {
+        playRound();
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    console.log('Reset');
+});
