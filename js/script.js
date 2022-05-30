2f5b84fef487d9c38b24f4e978bdd3affc031e93
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-05-30 14:09:33 +0800

    Add basic functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const btnXMark = document.getElementById('btn_xmark');
+const btnCircle = document.getElementById('btn_circle');
+const btnRefresh = document.getElementById('btn_refresh');
+const btnCells = document.querySelectorAll('.gameboard__cell');
+const labelCurrentTurn = document.getElementById('label_current_turn');
+const labelHumanMarker = document.getElementById('label_human_marker');
+const labelBotMarker = document.getElementById('label_bot_marker');
+const labelHumanScore = document.getElementById('label_human_score');
+const labelBotScore = document.getElementById('label_bot_score');
+const labelTieScore = document.getElementById('label_tie_score');
+
 const gameboard = (() => {
     let state = [null, null, null,
                  null, null, null,
@@ -5,93 +16,98 @@ const gameboard = (() => {
     let turn = 1;
 
     const getState = () => state;
+    const setState = (index, marker) => {
+        state.splice(index, 1, marker);
+    };
+
     const getTurn = () => turn;
-
-    const setState = (index, turn) => {
-        state[index] = turn;
-    };
-
     const setTurn = () => {
-        turn = turn + 1;
+        turn++;
     };
 
-    return {getState, getTurn, setState, setTurn};
+    const update = () => {
+        btnCells.forEach(element => {
+            switch(state[element.dataset.index]) {
+                case 'X':
+                    element.innerHTML = `<i class="fa-solid fa-xmark fa-xl"></i>`;
+                    break;
+                case 'O':
+                    element.innerHTML = `<i class="fa-regular fa-circle"></i>`;
+                    break;
+                default:
+                    element.innerHTML = ``;
+            }
+        });
+    };
+
+    const reset = () => {
+        state = [null, null, null,
+                 null, null, null,
+                 null, null, null];
+    }
+
+    const gameOver = () => {
+        return isWinner(0, 1, 2) 
+        ||  isWinner(3, 4, 5) 
+        ||  isWinner(6, 7, 8) 
+        ||  isWinner(0, 3, 6) 
+        ||  isWinner(1, 4, 7) 
+        ||  isWinner(2, 5, 8) 
+        ||  isWinner(0, 4, 8) 
+        ||  isWinner(6, 4, 2)
+        ||  isTie();   
+    }
+
+    const isWinner = (p1, p2, p3) => {
+        const c1 = state[p1];
+        if (c1 === null) return false;
+
+        const c2 = state[p2];
+        if (c1 !== c2) return false;
+
+        const c3 = state[p3];
+        if (c1 != c3) return false;
+        
+        return true;
+    }
+
+    const isTie = () => {
+        for (let i = 0; i < state.length; i++) {
+            if (state[i] === null) return false;
+        }
+        return 'Tie';
+    }
+
+    return {getState, setState, getTurn, setTurn, update, reset, gameOver};
 })();
 
-const Player = () => {
-    let playerMarker;
-    const score = 0;
-    const setPlayerMarker = (marker) => {
-        marker == 'x' ? playerMarker = 'x' : playerMarker = 'o';
-    };
-    const getPlayerMarker = () => playerMarker;
-    const setScore = () => score++;
-    const getScore = () => score;
-
-    return {getPlayerMarker, setPlayerMarker, getScore, setScore};
-};
-
-const xBtn = document.getElementById('btn_x');
-const oBtn = document.getElementById('btn_o');
-const resetBtn = document.getElementById('btn_reset');
-const cells = document.querySelectorAll('.gameboard_cell');
-const human = Player();
-const cpu = Player();
-
-const playRound = () => {
-    if (gameboard.getTurn() >= 9) {
-        // TODO: Display to the DOM, the game is tie
-        // Show restart button
-        console.log('Tie');
-    }
-    // Checks if current turn is odd
-    if (gameboard.getTurn() % 2 == 1) {
-        console.log(`I'm in Player 1`);
-        cells.forEach((cell) => {
-            cell.addEventListener('click', (e) => {
-                appendTurn(e.target, human.getPlayerMarker());
-            });
-        });
-    } else {
-        console.log(`I'm in Player 2`);
-        cells.forEach((cell) => {
-            cell.addEventListener('click', (e) => {
-                appendTurn(e.target, cpu.getPlayerMarker());
-            });
-        });
+const isThereWinner = (pos) => {
+    switch (gameboard.gameOver()) {
+        case true:
+            alert(`There's a winner!`);
+            break;
+        case false:
+            if (gameboard.getState()[pos] !== null) return;
+            gameboard.getTurn() % 2 === 1 ? gameboard.setState(pos, 'X') : gameboard.setState(pos, 'O');
+            gameboard.setTurn();
+            gameboard.update();
+            break;
+        default:
+            alert(`It's a tie!`);
+            break;
     }
 };
 
-const appendTurn = (target, marker) => {
-    gameboard.setState(target.dataset.index, marker);
-    gameboard.setTurn();
-    console.log(gameboard.getState());
-    console.log(`Turn: ${gameboard.getTurn()}`);
-    playRound();
-}
-
-xBtn.addEventListener('click', (e) => {
-    if (human.getPlayerMarker() === undefined && cpu.getPlayerMarker() === undefined) {
-        human.setPlayerMarker('x');
-        cpu.setPlayerMarker('o');
-    }
-
-    if (gameboard.getTurn() == 1) {
-        playRound();
-    }
+btnCells.forEach(element => {
+    element.addEventListener('click', (e) => {
+        let pos = e.target.dataset.index;
+        if (e.target.childNodes.length !== 0) return;
+        isThereWinner(pos);
+        console.log(gameboard.getState());
+    });
 });
 
-oBtn.addEventListener('click', (e) => {
-    if (human.getPlayerMarker() === undefined && cpu.getPlayerMarker() === undefined) {
-        human.setPlayerMarker('o');
-        cpu.setPlayerMarker('x');
-    }
-
-    if (gameboard.getTurn() == 1) {
-        playRound();
-    }
-});
-
-resetBtn.addEventListener('click', () => {
-    console.log('Reset');
+btnRefresh.addEventListener('click', () => {
+    gameboard.reset();
+    gameboard.update();
 });
