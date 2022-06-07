585470c796e365a87d07eab7acb17b8f8997e915
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-06-07 17:21:30 +0800

    Add content to remaining pages

--- src/assets/pages/About.js
+++ src/assets/pages/About.js
@@ -3,9 +3,13 @@ const About = () => {
     main.classList.add('main');
     main.classList.add('about');
 
-    const para = document.createElement('p');
-    para.textContent = 'In the business since 1999.';
+    const heading = document.createElement('h1');
+    heading.textContent = 'About us';
 
+    const para = document.createElement('p');
+    para.textContent = 'We are focused on providing the best possible experience for our customers. When we’re working on a piano new or used, we take great care to ensure that it will ultimately serve you, rather than our own internal goals or bottom line. Our business is about dedication, being easy to do business with and helping you, the customer, apply our knowledge to your shopping, we are passionate about wanting to provide you with the best products, the best possible service and best possible price, we work tirelessly to do so.';
+
+    main.appendChild(heading);
     main.appendChild(para);
 
     return main;
--- src/assets/pages/Contact.js
+++ src/assets/pages/Contact.js
@@ -3,9 +3,13 @@ const Contact = () => {
     main.classList.add('main');
     main.classList.add('contact');
 
-    const para = document.createElement('p');
-    para.textContent = '0999-509-4450';
+    const heading = document.createElement('h1');
+    heading.textContent = 'Contact us';
 
+    const para = document.createElement('p');
+    para.textContent = '0999-509-4450 | hi.carlodacuyan@gmail.com';
+
+    main.appendChild(heading);
     main.appendChild(para);
 
     return main;
