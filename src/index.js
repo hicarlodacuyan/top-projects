f9709fe66f4a6475784fc43e101758b3d0cf68c4
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-06-12 13:58:31 +0800

    Setup bootstrap and add initial Input component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './main.scss';
 import Project from './utils/Projects';
+import Input from './components/Input';
 
-const project1 = new Project();
-project1.add('Wake up!');
-project1.add('Wash');
-project1.add('Coffee');
-project1.add('Sleep');
-project1.changeStatus(1);
-project1.changeStatus(2);
-project1.clearCompleted();
-console.log(project1);
+const component = () => {
+    return Input();
+};
+
+document.getElementById('content').appendChild(component());
