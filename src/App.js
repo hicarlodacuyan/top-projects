e88de772cbe52d85260c4000b022b5c13cedaba7
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-06-18 14:56:07 +0800

    Add initial list design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./assets/components/Header";
 import Input from "./assets/components/Input";
+import List from "./assets/components/List";
 
 function App() {
     return `
         <div class="app__container">
             ${Header()}
             ${Input()}
+            ${List()}
         </div>
     `;
 }
