4178941ac38ddc5c84b030801ab4ae20a1b135b9
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2022-06-19 16:09:15 +0800

    Back to clean slate

--- src/assets/components/Input.js
+++
@@ -1,8 +0,0 @@
-export default function Input() {
-    return `
-        <div class="app__input">
-            <button class="input_btn">+</button>
-            <input class="input" type="text">
-        </div>
-    `;
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
 import './main.scss';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-rootElement.innerHTML = App();
