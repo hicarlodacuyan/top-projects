c8777448c3b44a389bb0b592e0b5c79a6b3817b6
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-06-23 15:02:54 +0800

    Prevent adding empty todo

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -28,6 +28,7 @@ const handleFormSubmit = event => {
     event.preventDefault();
     let input = document.getElementById('content');
     let newItem = input.value;
+    if(newItem === '') return;
     input.value = '';
 
     observable.notify('itemAdded', newItem);
