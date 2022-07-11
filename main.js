4ae9f254b7f463ea4c20fb6a19741d87c933d0d9
hicarlodacuyan <hi.carldacuyan@gmail.com>
2022-07-11 10:43:29 +0800

    Converted Promise code to async/await version

--- main.js
+++ main.js
@@ -2,20 +2,17 @@ const img = document.querySelector('img');
 const btn = document.querySelector('button');
 const searchBox = document.querySelector('input');
 
-const getNewGif = (query) => {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=12PiUjNAbWGBbK6bsVjQGYvITaSunyqR&s=${query}`, {
-        mode: 'cors'
-    })
-        .then(response => {
-            return response.json();
-        })
-        .then(response => {
-            img.src = response.data.images.original.url;
-            searchBox.value = '';
-        })
-        .catch(err => {
-            alert(`We can't find any gifs with the query you've provided. Please try again!`);
-        });
+const getNewGif = async (query) => {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=12PiUjNAbWGBbK6bsVjQGYvITaSunyqR&s=${query}`, {mode: 'cors'});
+    const gifData = await response.json();
+
+    try {
+        img.src = gifData.data.images.original.url;
+        searchBox.value = '';
+    } catch (err) {
+        alert(`We can't find GIFs with that name!`);
+        searchBox.value = '';
+    }
 };
 
 getNewGif('cat');
