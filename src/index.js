1f64efe08cabcfdf9587bd39b3fce9853982a429
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-07-28 09:45:23 +0800

    Rebase to July 25, 2022 commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import "./style.css";
-import shipFactory from "./factories/shipFactory";
-import gameboardFactory from "./factories/gameboardFactory";
-
-const playerBoard = gameboardFactory();
-
-const carrier = shipFactory(5);
-
-playerBoard.placeShip(0, 0, carrier);
-console.log(playerBoard.board);
-
-playerBoard.receiveAttack(0, 0);
-console.log(playerBoard.board);
