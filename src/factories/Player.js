97fac039af9ef1a7f933196d0749fe100e3eba06
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-08-02 10:08:55 +0800

    Add Player class

---
+++ src/factories/Player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+
+  /**
+   * Get the name of the Player
+   * @returns the value of the name variable
+   */
+  getName() {
+    return this.name;
+  }
+}
