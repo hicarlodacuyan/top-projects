021c170a08f3d6fc7a6f6f4683ececa7b5072d96
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-08-08 15:51:03 +0800

    Add basic functionality

--- src/components/boardComponent.js
+++ src/components/boardComponent.js
@@ -1,11 +1,39 @@
-import Observable from "../lib/Observable";
-
-export default function boardComponent(theBoard) {
+export default function boardComponent(theBoard, player) {
   return theBoard.fieldStatus
     .map((row, coordX) =>
-      row.map((col, coordY) => `<div class="board-cell"></div>`).join("")
+      row
+        .map((col, coordY) => {
+          if (theBoard.getFieldStatus(coordX, coordY) === 1) {
+            return `
+              <div class="board-cell-${player}">
+                <div class="hit-missed-player-1"></div>
+              </div>
+            `;
+          }
+
+          if (theBoard.getFieldStatus(coordX, coordY) === 2) {
+            return `
+              <div class="board-cell-${player}">
+                <div class="ship-body">
+                  <div class="occupied-not-hit"></div>
+                </div>
+              </div>
+            `;
+          }
+
+          if (theBoard.getFieldStatus(coordX, coordY) === 3) {
+            return `
+              <div class="board-cell-${player}">
+                <div class="ship-body">
+                  <div class="hit"></div>
+                </div>
+              </div>
+            `;
+          }
+
+          return `<div class="board-cell-${player}"></div>`;
+        })
+        .join("")
     )
     .join("");
 }
-
-Observable.subscribe("boardUpdated", boardComponent);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,142 @@ import Board from "./factories/Board";
 import Ship from "./factories/Ship";
 import Coordinate from "./factories/Coordinate";
 import boardComponent from "./components/boardComponent";
+import randomNumber from "./lib/randomNumber";
 
 const playerBoard = new Board(10);
-render(boardComponent(playerBoard), document.querySelector(".board-player-1"));
+const cruiser = new Ship(
+  [new Coordinate(0, 0), new Coordinate(0, 1)],
+  "cruiser"
+);
+const submarine = new Ship(
+  [new Coordinate(2, 0), new Coordinate(2, 1), new Coordinate(2, 2)],
+  "submarine"
+);
+const destroyer = new Ship(
+  [new Coordinate(4, 0), new Coordinate(4, 1), new Coordinate(4, 2)],
+  "destroyer"
+);
+const battleship = new Ship(
+  [
+    new Coordinate(6, 0),
+    new Coordinate(6, 1),
+    new Coordinate(6, 2),
+    new Coordinate(6, 3),
+  ],
+  "battleship"
+);
+const carrier = new Ship(
+  [
+    new Coordinate(8, 0),
+    new Coordinate(8, 1),
+    new Coordinate(8, 2),
+    new Coordinate(8, 3),
+    new Coordinate(8, 4),
+  ],
+  "carrier"
+);
+playerBoard.placeShip(cruiser);
+playerBoard.placeShip(submarine);
+playerBoard.placeShip(destroyer);
+playerBoard.placeShip(battleship);
+playerBoard.placeShip(carrier);
+render(
+  boardComponent(playerBoard, 1),
+  document.querySelector(".board-player-1")
+);
 
 const AIBoard = new Board(10);
-render(boardComponent(AIBoard), document.querySelector(".board-player-2"));
+const cruiser2 = new Ship(
+  [new Coordinate(0, 0), new Coordinate(0, 1)],
+  "cruiser"
+);
+const submarine2 = new Ship(
+  [new Coordinate(2, 0), new Coordinate(2, 1), new Coordinate(2, 2)],
+  "submarine"
+);
+const destroyer2 = new Ship(
+  [new Coordinate(4, 0), new Coordinate(4, 1), new Coordinate(4, 2)],
+  "destroyer"
+);
+const battleship2 = new Ship(
+  [
+    new Coordinate(6, 0),
+    new Coordinate(6, 1),
+    new Coordinate(6, 2),
+    new Coordinate(6, 3),
+  ],
+  "battleship"
+);
+const carrier2 = new Ship(
+  [
+    new Coordinate(8, 0),
+    new Coordinate(8, 1),
+    new Coordinate(8, 2),
+    new Coordinate(8, 3),
+    new Coordinate(8, 4),
+  ],
+  "carrier"
+);
+AIBoard.placeShip(cruiser2);
+AIBoard.placeShip(submarine2);
+AIBoard.placeShip(destroyer2);
+AIBoard.placeShip(battleship2);
+AIBoard.placeShip(carrier2);
+render(boardComponent(AIBoard, 2), document.querySelector(".board-player-2"));
+
+function handlePlayerTurn() {
+  document.querySelectorAll(".board-cell-2").forEach((cell, index) => {
+    cell.addEventListener("click", (e) => {
+      let coords = Array.from(String(index), Number);
+      if (coords.length === 1) {
+        coords.unshift(0);
+      }
+
+      AIBoard.placeShot(new Coordinate(coords[0], coords[1]));
+      AIBoard.getFleetStatus();
+      render(
+        boardComponent(AIBoard, 2),
+        document.querySelector(".board-player-2")
+      );
+
+      if (AIBoard.isGameOver()) {
+        alert(`Game Over! You won!`);
+        return;
+      }
+
+      setTimeout(() => {
+        handleOpponentTurn();
+      }, 1000);
+    });
+  });
+}
+
+function handleOpponentTurn() {
+  const coords = new Coordinate(randomNumber(0, 9), randomNumber(0, 9));
+
+  if (!playerBoard.canPlaceShot(coords)) {
+    console.log(`Can't place shot there! Trying again.`);
+    handleOpponentTurn();
+  } else {
+    try {
+      playerBoard.placeShot(coords);
+      playerBoard.getFleetStatus();
+
+      render(
+        boardComponent(playerBoard, 1),
+        document.querySelector(".board-player-1")
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (playerBoard.isGameOver()) {
+    alert("Game Over! AI won.");
+    return;
+  }
+
+  handlePlayerTurn();
+}
+
+handlePlayerTurn();
