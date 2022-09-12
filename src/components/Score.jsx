ea97670cb79b3ab3ee158520e3e97269b1035260
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-09-12 13:51:14 +0800

    Add footer styling

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,35 +1,15 @@
 import React from "react";
-
-const scoreStyle = {
-  display: "flex",
-  border: "5px solid lightblue"
-};
-
-const currentScoreStyle = {
-  border: "5px solid darkblue", 
-  flex: "1" 
-};
-
-const highestScoreStyle = {
-  border: "5px solid lightgreen", 
-  flex: "1" 
-};
+import "../styles/Score.css";
 
 const Score = () => {
   return (
-    <section
-      style={scoreStyle}
-    >
-      <div 
-        style={currentScoreStyle}
-      >
-        Score: 26
+    <section className="scores-container">
+      <div>
+        Score:&nbsp; <span className="current-score-points">26</span>
       </div>
 
-      <div 
-        style={highestScoreStyle}
-      >
-        High Score: 30
+      <div>
+        High Score:&nbsp; <span className="current-score-points">30</span>
       </div>
     </section>
   );
