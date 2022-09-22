9d4ffb3786eebd7747ee1c0dd2d1286f76f2bf7a
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-09-22 13:15:33 +0800

    Delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="py-32 text-center">
       <h1 className="text-4xl font-extrabold">Home</h1>
     </div>
   );
-}
+};
 
 export default App;
