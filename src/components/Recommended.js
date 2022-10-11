5c6186502b1415666f1cd3b50025a52c0be7b750
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-10-11 16:51:47 +0800

    Fix responsive class for 1536px devices & up

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -5,7 +5,7 @@ const Recommended = ({ posterSize }) => {
   return (
     <div className="flex-1 flex flex-col gap-2">
       <h1 className="text-2xl">Recommended</h1>
-      <div className="grid xl:grid-cols-8 xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-4">
+      <div className="grid 2xl:grid-cols-8 xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-4">
         <Movie posterSize={posterSize} />
         <Movie posterSize={posterSize} />
         <Movie posterSize={posterSize} />
