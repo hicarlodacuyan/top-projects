efdf6996151496561656ca462c808756ff5a965b
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-10-11 16:36:27 +0800

    Add navbar basic styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,32 @@
 import React from "react";
+import {
+  MdMovieCreation,
+  MdViewQuilt,
+  MdLocalMovies,
+  MdTv,
+  MdOutlineBookmark,
+  MdAccountCircle,
+} from "react-icons/md";
+
+const profileIcon = { color: "#586488", fontSize: "1.5em" };
+const primaryIcon = { color: "#FF4B4A", fontSize: "1.5em" };
+const secondaryIcon = { color: "#586488", fontSize: "1.25em" };
 
 const Nav = () => {
   return (
-    <nav className="bg-slate-800 md:max-h-96 md:rounded-md p-4">
-      <h1>Nav</h1>
+    <nav className="flex lg:flex-col flex-row items-center justify-between bg-slate-800 md:max-h-96 md:rounded-md p-4">
+      <div>
+        <MdMovieCreation style={primaryIcon} />
+      </div>
+      <div className="flex lg:flex-col gap-4">
+        <MdViewQuilt style={secondaryIcon} />
+        <MdLocalMovies style={secondaryIcon} />
+        <MdTv style={secondaryIcon} />
+        <MdOutlineBookmark style={secondaryIcon} />
+      </div>
+      <div>
+        <MdAccountCircle style={profileIcon} />
+      </div>
     </nav>
   );
 };
