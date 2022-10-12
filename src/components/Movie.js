9ca42354e2cb24d8b9240a5c9100eeef7e89c6e4
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-10-12 15:26:06 +0800

    Add recommended movies mock details

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdLocalMovies } from "react-icons/md";
 
 const Movie = ({ posterSize }) => {
   return (
@@ -13,13 +14,23 @@ const Movie = ({ posterSize }) => {
           <h2 className="text-center">Title</h2>
         </div>
       ) : (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <img
             className="rounded-md"
             src={`https://via.placeholder.com/${posterSize}`}
             alt="placeholder"
           />
-          <h2 className="text-center">Title</h2>
+          <div className="flex gap-1 text-xs text-slate-300">
+            <p>2022</p>
+            <span>•</span>
+            <p className="flex justify-center items-center gap-1">
+              <MdLocalMovies />
+              <span>Movie</span>
+            </p>
+            <span>•</span>
+            <p>18+</p>
+          </div>
+          <h2 className="font-bold">Movie Title</h2>
         </div>
       )}
     </>
