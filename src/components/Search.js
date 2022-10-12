c20d79bbae1e956ddbdc189b4716e0dfaca15f4c
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-10-12 17:06:26 +0800

    Add search icon & placeholder value to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { MdSearch } from "react-icons/md";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="flex items-center gap-4">
+      <MdSearch fontSize="1.5em" color="#586488" />
+      <span className="text-slate-300">Search for Movies or TV Series</span>
+    </div>
+  );
 };
 
 export default Search;
